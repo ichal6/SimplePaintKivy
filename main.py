0be34e2e28b405a91ee129aeb0791ebc78066072
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-05-21 17:19:56 +0200

    Add clear button

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Line
+from kivy.uix.button import Button
 
 
 class MyPaintWidget(Widget):
 
     def on_touch_down(self, touch):
-        color = (random(), random(), random())
+        color = (random(), 1, 1)
         with self.canvas:
-            Color(*color)
+            Color(*color, mode="hsv")
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             touch.ud['line'] = Line(points=(touch.x, touch.y))
@@ -21,7 +22,16 @@ class MyPaintWidget(Widget):
 class MyPaintApp(App):
 
     def build(self):
-        return MyPaintWidget()
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 
 if __name__ == '__main__':
